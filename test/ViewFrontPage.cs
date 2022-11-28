7c2067bcdfd3bd0299bc26a2e5504c9a42f3d2e3
Joshua Mitchell <jmitchel021@student.wethinkcode.co.za>
2022-11-28 13:38:03 +0200

    ahhhhhhhhhh

--- test/ViewFrontPage.cs
+++ test/ViewFrontPage.cs
@@ -11,16 +11,18 @@ public class ViewFrontPage
 
     [SetUp]
     public void Setup()
-    {ChromeOptions options = new ChromeOptions();
+    {
+        ChromeOptions options = new ChromeOptions();
         options.AddArgument("headless");
         driver = new ChromeDriver(options);
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
     }
+
     [Test]
     public void FrontPageTest()
     {
         driver.Navigate().GoToUrl("http://localhost:90");
-        var title = driver.Title;
+        var title = driver.Title; 
         Assert.That(title, Is.EqualTo("Your store. Home page title"));
         var altText = driver.FindElement(By.XPath("/html/body/div[6]/div[1]/div[2]/div[1]/a/img")).GetAttribute("alt");
         Assert.That(altText, Is.EqualTo("Your store name"));
